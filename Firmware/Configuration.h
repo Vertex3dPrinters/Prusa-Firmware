c17c7992105c1dc6c0ca39c05fa70bfa607cc24c
jfestrada <jfestrada@cloudhd.eu>
2021-06-18 16:17:38 +0200

    Updated the commit number

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -17,7 +17,7 @@ extern PGM_P sPrinterName;
 
 // Firmware version
 #define FW_VERSION "3.10.0"
-#define FW_COMMIT_NR 5588
+#define FW_COMMIT_NR 5589
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
